a5dd32f6ea1facdfa3ffed3e4f6bda02da10fb06
Henrique Prange <hprange@gmail.com>
2013-02-14 00:25:58 -0200

    Transforma paths em caminhos relativos

--- config.rb
+++ config.rb
@@ -63,6 +63,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :relative_links, true
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -75,7 +77,7 @@ configure :build do
   # activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
